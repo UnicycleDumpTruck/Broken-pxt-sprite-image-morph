875607e67c1b940488113227fa23b5daafcd93d1
Jeff Highsmith <junk@jeffhighsmith.com>
2020-08-01 09:27:51 -0400

    Fixed random, stopped making 256 extra sprites.

--- main.ts
+++ main.ts
@@ -1,16 +1,13 @@
 function morph (mySprite: Sprite, myImage: Image) {
-    start_img = mySprite.image
-    end_img = myImage
     pixels_to_change = []
     for (let index = 0; index <= 255; index++) {
         pixels_to_change.push(index)
     }
     while (pixels_to_change.length > 0) {
-        random_pixel = pixels_to_change.removeAt(randint(0, pixels_to_change.length))
+        random_pixel = pixels_to_change.removeAt(randint(0, pixels_to_change.length - 1))
         row = Math.floor(random_pixel / 16)
         column = random_pixel % 16
-        start_img.setPixel(column, row, end_img.getPixel(column, row))
-        mySprite = sprites.create(start_img, SpriteKind.Player)
+        mySprite.image.setPixel(column, row, myImage.getPixel(column, row))
         pause(0.01)
     }
 }
@@ -18,9 +15,6 @@ let column = 0
 let row = 0
 let random_pixel = 0
 let pixels_to_change: number[] = []
-let end_img: Image = null
-let mySprite: Sprite = null
-let start_img: Image = null
 let player_sprite = sprites.create(sprites.food.smallApple, SpriteKind.Player)
 game.showLongText("Press A to start morph.", DialogLayout.Bottom)
 morph(player_sprite, sprites.food.smallLemon)
